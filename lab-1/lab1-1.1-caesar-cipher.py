31cc6c7c9d1e096474dad3516d4707368a0d4f2f
Zontzip <zontzor@gmail.com>
2016-09-13 18:13:30 +0100

    Add lab 1 - Caesar cipher

---
+++ lab-1/lab1-1.1-caesar-cipher.py
@@ -0,0 +1,22 @@
+def caesar(s, k, decrypt=False):
+	if decrypt: k = 26 - k
+	r = ""
+	for i in s:
+		if(ord(i) >= 65 and ord(i) <= 90):
+			r += chr((ord(i) - 65 + k) % 26 + 65)
+		elif(ord(i) >= 97 and ord(i) <= 122):
+			r += chr((ord(i) - 97 + k) % 26 + 97)
+		else:
+			r += i
+	return r
+
+def encrypt(p, k):
+	return caesar(p, k)
+
+def decrypt(c, k):
+	return caesar(c, k, True)	
+
+c = caesar("hello alex", 16, False)
+print c
+
+print decrypt(c, 16)
