274baa17adcf1c51450ea7f600d4185f5b228906
Zontzip <zontzor@gmail.com>
2016-09-14 20:05:00 +0100

    Add comments to rail fence

--- lab-1/1.2-rail-fence-cipher.py
+++ lab-1/1.2-rail-fence-cipher.py
@@ -1,22 +1,35 @@
-def fence(p, k):
-	fence = [[None] * len(p) for n in range(k)]
-	rails = range(k - 1) + range(k - 1, 0, -1)
+# Date created: 2016-09-13 
+# Authour: Alex Kiernan
+# Contents: Rail fence cipher implementation
+import pdb
 
-	for n, x in enumerate(p):
+def fence(msg, key):
+	pdb.set_trace()
+	# Create 2D array of size [key] of arrays of size [msg] 
+	# which forms the fence
+	fence = [[None] * len(msg) for n in range(key)]
+	# Create array of values corresponding to the range of the key - 1 and 
+	# the range of values of the key - 1 to 0 with a step down of - 1
+	rails = range(key - 1) + range(key - 1, 0, -1)
+
+	# enumerate returns an enumerate object which is a number corresponding 
+	# to each letter in the msg
+	for n, x in enumerate(msg):
+		# fill in the 2D array with the msg
 		fence[rails[n % len(rails)]][n] = x
 
 	return [c for rail in fence for c in rail if c is not None]
 
-def encrypt(p, k):
-	return ''.join(fence(p, k))
+def encrypt(msg, key):
+	return ''.join(fence(msg, key))
 
-def decrypt(c, k):
+def decrypt(c, key):
 	rng = range(len(c))
-	pos = fence(rng, k)
-	return ''.join(c[pos.index(k)] for k in rng)
+	pos = fence(rng, key)
+	return ''.join(c[pos.index(key)] for key in rng)
 
+# Encrypt a message with a fence of n
 f = encrypt("Hello", 3)
 print f
 
 print decrypt(f, 3)
- 
