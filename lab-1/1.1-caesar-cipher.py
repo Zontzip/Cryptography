16e992c709667148c1916709747676269065e033
Zontzip <zontzor@gmail.com>
2016-09-14 10:28:40 +0100

    Add explanatory comments to caesar cipher

--- lab-1/1.1-caesar-cipher.py
+++ lab-1/1.1-caesar-cipher.py
@@ -1,23 +1,45 @@
-def caesar(s, k, decrypt=False):
+# Date created: 2016-09-13 
+# Authour: Alex Kiernan
+# Contents: Caesar cipher implementation
+
+#import pdb
+
+# pass in the msg, key and optional decrypt (default is False)
+def caesar(string, key, decrypt=False):
+	#pdb.set_trace()
+
+	# if true, the algorithm will be reversed
 	if decrypt: 
-		k = 26 - k
-	r = ""
-	for i in s:
-		if(ord(i) >= 65 and ord(i) <= 90):
-			r += chr((ord(i) - 65 + k) % 26 + 65)
-		elif(ord(i) >= 97 and ord(i) <= 122):
-			r += chr((ord(i) - 97 + k) % 26 + 97)
+		key = 26 - key
+
+	encrypted_msg = ""
+	# 1. Loop through each letter in the string.
+	# 2. Check if letter is between A - Z (65 - 90)
+	# 	 or else is between a - z (97 - 122).
+	# 3. If character is between either of these than append a random letter 
+	#    that is between either 65 - 90 or 97 - 122 to the encrypted_msg string.
+	# For example, h (ascii 104) would enter the second "if" and the character 
+	# x (ascii 120) would be added to the encrypted_msg string.
+	for letter in string:
+		# ord() returns the ascii representation of a character
+		if(ord(letter) >= 65 and ord(letter) <= 90):
+			# chr() is the opposite of ord(), returns a character
+			encrypted_msg += chr((ord(letter) - 65 + key) % 26 + 65)
+		elif(ord(letter) >= 97 and ord(letter) <= 122):
+			encrypted_msg += chr((ord(letter) - 97 + key) % 26 + 97)
+		# else do not encrypt the letter
 		else:
-			r += i
-	return r
+			encrypted_msg += letter
+	return encrypted_msg
 
-def encrypt(p, k):
-	return caesar(p, k)
+def encrypt(unencrypted_msg, key):
+	return caesar(unencrypted_msg, key)
 
-def decrypt(c, k):
-	return caesar(c, k, True)	
+def decrypt(c, key):
+	return caesar(c, key, True)	
 
-c = caesar("hello alex", 16, False)
-print c
+# Pass in message and number of places to shift each letter
+msg = encrypt("hello alex", 16)
+print msg
 
-print decrypt(c, 16)
+print decrypt(msg, 16)
