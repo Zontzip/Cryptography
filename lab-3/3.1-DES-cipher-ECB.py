914a3214d3b592760326e30329eaf62d26a5f007
Alex Kiernan <zontzor@gmail.com>
2016-10-03 09:46:42 +0100

    Add lab 3 part 1 solution

--- lab-3/3.1-DES-cipher-ECB.py
+++ lab-3/3.1-DES-cipher-ECB.py
@@ -6,9 +6,11 @@ key = "12345678"
 plain_text = "AAAABBBBAAAABBBB"
 encrypted_msg = "19FF4637BB2FE77C19FF4637BB2FE77C"
 
-des1 = DES.new(key, DES.MODE_ECB)
+des = DES.new(key, DES.MODE_ECB)
 cipher_text = des.encrypt(plain_text)
-print(cipher_text.decode)
+cipher_text = cipher_text.encode('hex');
+print(cipher_text)
 
-des2 = DES.new(key, DES.MODE_ECB)
-decrypted_msg = des.decrypt(encrypted_msg)
+cipher_text = cipher_text.decode('hex');
+decrypted_msg = des.decrypt(cipher_text)
+print(decrypted_msg)
