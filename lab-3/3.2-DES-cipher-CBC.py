21aaa301d7915c460568132fea878879b14e0b13
Zontzip <zontzor@gmail.com>
2016-10-03 23:53:51 +0100

    Started lab 3 part 2 - ran into problem with first block of decrypred msg being random

--- lab-3/3.2-DES-cipher-CBC.py
+++ lab-3/3.2-DES-cipher-CBC.py
@@ -3,10 +3,11 @@
 from Crypto.Cipher import DES
 	
 key = "12345678"
+iv = "00000000"
 plain_text = "AAAABBBBAAAABBBB"
-cipher_text = "19FF4637BB2FE77C19FF4637BB2FE77C"
+cipher_text = "AAC823F6BBE58F9EAF1FE0EB9CA7EB08"
 
-des = DES.new(key, DES.MODE_ECB)
+des = DES.new(key, DES.MODE_CBC, iv)
 encrypted_msg = des.encrypt(plain_text)
 encrypted_msg = encrypted_msg.encode('hex');
 print(encrypted_msg)
