2c3bfcd32525aa594c6c2cf9dca33303f769bcea
Zontzip <zontzor@gmail.com>
2016-10-04 13:04:27 +0100

    Finish lab 3 part 2

--- lab-3/3.2-DES-cipher-CBC.py
+++ lab-3/3.2-DES-cipher-CBC.py
@@ -7,11 +7,12 @@ iv = "00000000"
 plain_text = "AAAABBBBAAAABBBB"
 cipher_text = "AAC823F6BBE58F9EAF1FE0EB9CA7EB08"
 
-des = DES.new(key, DES.MODE_CBC, iv)
-encrypted_msg = des.encrypt(plain_text)
+des1 = DES.new(key, DES.MODE_CBC, iv)
+encrypted_msg = des1.encrypt(plain_text)
 encrypted_msg = encrypted_msg.encode('hex');
 print(encrypted_msg)
 
+des2 = DES.new(key, DES.MODE_CBC, iv)
 encrypted_msg = encrypted_msg.decode('hex');
-decrypted_msg = des.decrypt(encrypted_msg)
+decrypted_msg = des2.decrypt(encrypted_msg)
 print(decrypted_msg)
