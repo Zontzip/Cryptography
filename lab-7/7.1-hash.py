9af4043a0e8d99dbe7202cd360d9562910acf7fd
Zontzip <zontzor@gmail.com>
2016-11-28 23:50:11 +0000

    Clean lab 7

---
+++ lab-7/7.1-hash.py
@@ -0,0 +1,24 @@
+from Crypto.Cipher import DES
+import Padding
+
+key = "12345678"
+plaintext = "AAAABBBBCCCC"
+plaintext_with_padding = "AAAABBBBCCCC\x00\x0004"
+ciphertext_base_16 = "19FF4637BB2FE77C81987E5CB99B66E2"
+
+def pad(plaintext):
+	return Padding.appendPadding(plaintext,blocksize=Padding.DES_blocksize,mode='CMS')
+
+
+def unpad(padded_plaintext):
+	return Padding.removePadding(padded_plaintext,mode='CMS')
+
+print("Plaintext: " + plaintext)
+
+padded_plaintext = pad(plaintext)
+print("Padded plaintext: " + padded_plaintext)
+
+des = DES.new(key, DES.MODE_ECB)
+encrypted_msg = des.encrypt(padded_plaintext)
+encrypted_msg = encrypted_msg.encode('hex')
+print("Encrypted message: " + encrypted_msg)
