eb6b3d55c54618bcab42c7c5f669a21c0ebca134
Zontzip <zontzor@gmail.com>
2016-12-02 00:50:17 +0000

    Refactor lab 8 diffie

--- lab-8/8.1-diffie-hellman.py
+++ lab-8/8.1-diffie-hellman.py
@@ -2,28 +2,28 @@ import base64
 import hashlib
 import sys
 
-print("Diffie-Hellman Key Agreement")
+if __name__ == "__main__":
+    print("Diffie-Hellman Key Agreement")
 
-g = 5
-p = 23
+    g = 5
+    p = 23
+    print ("\nAlice and Bob agree prime modulus: " + str(p) + ", base: " + str(g))
 
-print ("\nAlice and Bob agree prime modulus: " + str(p) + ", base: " + str(g))
+    a = 6
+    b = 15
 
-a = 6
-b = 15
+    A = (g**a) % p
+    B = (g**b) % p
+    print ("\nAlice chooses a secret integer: " + str(a))
+    print ("Alice sends Bob: " + str(A))
 
-A = (g**a) % p
-B = (g**b) % p
-print ("\nAlice chooses a secret integer: " + str(a))
-print ("Alice sends Bob: " + str(A))
+    print ("\nBob chooses a secret integer: " + str(b))
+    print ("Bob sends Alice: " + str(B))
 
-print ("\nBob chooses a secret integer: " + str(b))
-print ("Bob sends Alice: " + str(B))
+    keyA = (B**a) % p
+    print ("\nAlice computes: " + str(keyA))
+    print ("Key: " + str(hashlib.sha256(str(keyA)).hexdigest()))
 
-keyA = (B**a) % p
-print ("\nAlice computes: " + str(keyA))
-print ("Key: " + str(hashlib.sha256(str(keyA)).hexdigest()))
-
-keyB = (A**b) % p
-print ("\nBob computes: " + str(keyB))
-print ("Key: "  + str(hashlib.sha256(str(keyB)).hexdigest()))
+    keyB = (A**b) % p
+    print ("\nBob computes: " + str(keyB))
+    print ("Key: "  + str(hashlib.sha256(str(keyB)).hexdigest()))
