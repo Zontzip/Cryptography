6d78f8d2d1fd5709e4a162cb0e8e26c9fc9c31d9
Zontzip <zontzor@gmail.com>
2016-12-02 00:45:15 +0000

    Refactor lab 4 AES

--- lab-4/4.1-AES-ECB.py
+++ lab-4/4.1-AES-ECB.py
@@ -1,25 +1,28 @@
 from Crypto.Cipher import AES
 import Padding
 
-key = "1234567812345678"
-plaintext = "AAAABBBBCCCCDDDDAA"
-plaintext_with_padding = "AAAABBBBCCCCDDDDAA\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x0014"
-ciphertext_base_16 = "43D3215C92A75A1478FCF9CB950D20DB502A485FD5735486D57AEA9AA809E3DD"
-
 def pad(plaintext):
-	return Padding.appendPadding(plaintext,blocksize=Padding.AES_blocksize,mode='ZeroLen')
+	return Padding.appendPadding(plaintext,blocksize=Padding.AES_blocksize,
+	mode='ZeroLen')
 
 def unpad(padded_plaintext):
 	return Padding.removePadding(padded_plaintext,mode='ZeroLen')
 
-print("Plaintext: " + plaintext)
+if __name__ == "__main__":
+	print("AES cipher in ECB mode")
+	key = "1234567812345678"
+	plaintext = "AAAABBBBCCCCDDDDAA"
+	plaintext_with_padding = "AAAABBBBCCCCDDDDAA\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x0014"
+	ciphertext_base_16 = "43D3215C92A75A1478FCF9CB950D20DB502A485FD5735486D57AEA9AA809E3DD"
 
-padded_plaintext = pad(plaintext)
-print("Padded plaintext: " + padded_plaintext.encode('hex'))
+	print("\nPlaintext: " + plaintext)
 
-aes = AES.new(key, AES.MODE_ECB)
-encrypted_msg = aes.encrypt(padded_plaintext)
-print("Encrypted message: " + encrypted_msg.encode('hex'))
+	padded_plaintext = pad(plaintext)
+	print("Padded plaintext: " + padded_plaintext.encode('hex'))
 
-decrypted_msg = aes.decrypt(encrypted_msg)
-print("Decrypted message: " + decrypted_msg)
+	aes = AES.new(key, AES.MODE_ECB)
+	encrypted_msg = aes.encrypt(padded_plaintext)
+	print("Encrypted message: " + encrypted_msg.encode('hex'))
+
+	decrypted_msg = aes.decrypt(encrypted_msg)
+	print("Decrypted message: " + decrypted_msg)
--- lab-4/4.2-AES-brute.py
+++ lab-4/4.2-AES-brute.py
@@ -1,7 +1,5 @@
 from Crypto.Cipher import AES
 
-ciphertext_base_16 = "43D3215C92A75A1478FCF9CB950D20DB"
-
 def pad(plaintext):
 	return Padding.appendPadding(plaintext,blocksize=Padding.AES_blocksize,mode='CMS')
 
@@ -11,14 +9,17 @@ def unpad(padded_plaintext):
 def is_ascii(s):
     return all(ord(c) < 128 for c in s)
 
-ciphertext_base_16 = ciphertext_base_16.decode('hex')
+if __name__ == "__main__":
+	print ("AES Brute Force")
+	ciphertext_base_16 = "43D3215C92A75A1478FCF9CB950D20DB"
+	ciphertext_base_16 = ciphertext_base_16.decode('hex')
 
-with open('keys.txt') as file:
-	keys = file.read().splitlines()
-file.closed
+	with open('keys.txt') as file:
+		keys = file.read().splitlines()
+	file.closed
 
-for key in keys:
-	aes = AES.new(key, AES.MODE_ECB)
-	decrypted_msg = aes.decrypt(ciphertext_base_16)
-	if is_ascii(decrypted_msg):
-		print("Decrypted message with key " + key + ": "+ decrypted_msg)
+	for key in keys:
+		aes = AES.new(key, AES.MODE_ECB)
+		decrypted_msg = aes.decrypt(ciphertext_base_16)
+		if is_ascii(decrypted_msg):
+			print("\nDecrypted message with key " + key + ": "+ decrypted_msg)
